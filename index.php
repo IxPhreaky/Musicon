027dd89c7d011311e34d7a56c2b98cb4ae46a860
IxPhreaky <mads_stokholm@hotmail.com>
2014-08-26 12:57:16 +0200

    Indsat billeder, index o gstyle

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Musicon</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="billeder/favicon (2).ico">
+</head>
+
+<body>
+	<div id="wrapper">
+    	<header>
+        	<img src="billeder/musicon-logo.png">
+                <nav>
+            		<ul>
+                		<li></li>
+                    	<li></li>
+                    	<li></li>
+                    	<li></li>
+           		  	</ul>
+       	 	 	</nav>
+        </header>
+        	<section>
+            	<article>
+                </article>
+           	</section>
+            	<footer>
+                </footer>
+    </div>
+</body>
+</html>
