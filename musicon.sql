ab42642018be6ec26f64c11abc41272fe56ae773
Tchikai <nikolaj1923@hotmail.com>
2014-08-27 09:11:18 +0200

    db

---
+++ musicon.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Vært: 127.0.0.1
+-- Genereringstid: 27. 08 2014 kl. 09:06:04
+-- Serverversion: 5.6.16
+-- PHP-version: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `musicon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `aktiviteter`
+--
+
+CREATE TABLE IF NOT EXISTS `aktiviteter` (
+  `overskrift` varchar(35) NOT NULL,
+  `aktivitet` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `logind`
+--
+
+CREATE TABLE IF NOT EXISTS `logind` (
+  `brugernavn` varchar(35) NOT NULL,
+  `kodeord` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `nyhed`
+--
+
+CREATE TABLE IF NOT EXISTS `nyhed` (
+  `overskrift` varchar(35) NOT NULL,
+  `nyhed` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `q&a`
+--
+
+CREATE TABLE IF NOT EXISTS `q&a` (
+  `spørgsmål` varchar(50) NOT NULL,
+  `svar` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
