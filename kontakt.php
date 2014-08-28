0bbc01be0a6625e4e3aa1db1cd3f38a586d88388
IxPhreaky <mads_stokholm@hotmail.com>
2014-08-28 08:28:31 +0200

    ASD

--- kontakt.php
+++ kontakt.php
@@ -46,25 +46,25 @@
 				
 				?>
 
-				<fieldset>
-<form method="post" action="">
-<ul>
-	<li>
-		<label for="Fulde_navn">Navn:</label>
-		<input name="Navn" type="text" placeholder="Skriv dit navn her" value="" required autofocus />
-	</li>
+	<fieldset>
+		<form method="post" action="">
+			<ul>
+				<li>
+					<label for="Fulde_navn">Navn:</label>
+					<input name="Navn" type="text" placeholder="Skriv dit navn her" value="" required autofocus />
+				</li>
 
-<li>
-<label for="Email">Email:</label>
-<input name="Email" type="text" placeholder="Jens@live.dk Etc." required pattern="^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,5})$" title="the given email adress is not valid" /></li>
+				<li>
+					<label for="Email">Email:</label>
+						<input name="Email" type="text" placeholder="Jens@live.dk Etc." required pattern=							"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,5})$" title="the given email adress is not valid" /></li>
 
-<li><label for="Mobil">Mobil Nummer:</label><input name="Mobil" pattern="[0-9]{8,8}"  type="tel" placeholder="12345678" required > </li>
+				<li><label for="Mobil">Mobil Nummer:</label><input name="Mobil" pattern="[0-9]{8,8}"  type="tel" placeholder="12345678" required > </li>
 
-<li><label for="Besked">Besked:</label><textarea name="Besked" cols="5" rows="5" placeholder="Skriv Her"></textarea> </li>
+				<li><label for="Besked">Besked:</label><textarea name="Besked" cols="5" rows="5" placeholder="Skriv Her"></textarea> </li>
 
-<li><button id="button" type="submit" name="submit" value="Send">Send</button></li>
+				<li><button id="button" type="submit" name="submit" value="Send">Send</button></li>
 
-</ul>
-</form>
-</fieldset>	
+			</ul>
+		</form>
+	</fieldset>	
 </html>
